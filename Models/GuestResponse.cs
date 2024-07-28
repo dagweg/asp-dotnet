db9fd28f3a9e4c0eccd957e06bd795bc7521fe8d
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-07-28 14:08:50 +0300

    rspv form & model

---
+++ Models/GuestResponse.cs
@@ -0,0 +1,11 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_dotnet.Models;
+
+public class GuestResponse {
+    public string? Name {get; set;}
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public bool? WillAttend { get; set; }
+}
